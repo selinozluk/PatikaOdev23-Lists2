d9c935b99f3cf128f8e63c7585dca8541959afda
selinozluk <selinozluk36@gmail.com>
2025-07-18 09:57:19 +0300

    Add project files.

---
+++ Lists2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Kahve isimlerini tutmak için boş liste
+List<string> coffeeList = new List<string>();
+
+// Kullanıcıdan 5 kahve ismi alır
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"Kahve {i + 1}: ");
+    string coffeeName = Console.ReadLine();
+    coffeeList.Add(coffeeName);
+}
+
+// Yazdırılacak olan kahve isimlerinin başlığı
+Console.WriteLine("\nGirilen Kahve İsimleri:");
+
+// Kahve isimlerini ekrana yazdırır
+foreach (var coffee in coffeeList)
+{
+    Console.WriteLine(coffee);
+}
